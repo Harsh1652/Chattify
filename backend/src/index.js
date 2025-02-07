029c751297038b6f8aaa984c4ca4904d023cfd22
Harsh <harsh160502@gmail.com>
2025-02-07 11:44:32 +0530

    MessageRoutes

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import messageRoutes from "./routes/message.route.js"; 
+
 
 dotenv.config()
 const app = express();
@@ -16,6 +18,8 @@ app.use(cookieParser());
 
 // Use the routes
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
 
 
 // Start the server
